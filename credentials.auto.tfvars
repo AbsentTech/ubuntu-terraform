484ad2446e2b8387afec4689413755ee2ad72b28
jessiesmith <jessiesmith@absenttech.com>
2023-05-26 12:10:19 -0400

    Changing Process

--- credentials.auto.tfvars
+++
@@ -1,6 +0,0 @@
-target_node="pve"
-vm_name="dockermain"
-cores=2
-memory=4000
-ipconfig="ip=10.1.2.121/24,gw=10.1.2.1" # Replace with new IP
-ciuser="absentadmin"
--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@
 # Create a new VM from a clone
 
 
-
 variable "target_node" {
   type = string
 }
@@ -31,24 +30,55 @@ variable "cipassword" {
   
 }
 
+variable "tags" {
+  type = string
+  default = "terraform,automation"
+}
 variable "sshkey" {
   type = string
   default = ""
 }
-variable "ipconfig" {
+variable "ipconfig0" {
   type = string
 }
 
-resource "proxmox_vm_qemu" "ubuntu-22" {
+variable "disk_size" {
+  type = string
+  default = "10G"
+}
 
-  boot        = "order=virtio0;ide2;ide0"
+variable "network_tag" {
+  type = number
+  default = 5
+}
+variable "pool" {
+  type = string
+  default = "Unassigned"
+}
+
+variable "vm_description" {
+  type = string
+  default = "Created by Terraform"
+}
+  
+variable "cloneTemplate" {
+  type = string
+  default = "UbuntuTemp"
+}
+  
+variable "vmid" {
+  type = number
+  default = 0
+}
+resource "proxmox_vm_qemu" "absentvm" {
+
   target_node = var.target_node
   name        = var.vm_name
-  desc        = "Main vm"
+  desc        = var.vm_description
   onboot      = true
-
-  clone = "UbuntuTemp"
-
+  vmid        = var.vmid
+  tags  = var.tags
+  clone = var.cloneTemplate
   # VM System Settings
   agent = 1
 
@@ -60,23 +90,36 @@ resource "proxmox_vm_qemu" "ubuntu-22" {
   # VM Memory Settings
   memory = var.memory
 
+  pool = var.pool
   # VM Network Settings
   network {
     bridge = "vmbr0"
     model  = "virtio"
+    tag = var.network_tag
   }
-
+ vga {
+    type = "serial0"
+  } 
+ #  create a disk with the size of 10GB
+  disk {
+    size = var.disk_size
+    type = "scsi"
+    storage = "local-lvm"
+    cache = "writeback"
+    iothread = 0
+  }
   # VM Cloud-Init Settings
   os_type = "cloud-init"
 
   # (Optional) IP Address and Gateway
-  ipconfig0  = var.ipconfig
+  ipconfig0  = var.ipconfig0
+  
+  # (Optional) Default User and Password
   ciuser     = var.ciuser
   cipassword = var.cipassword
-  # (Optional) Default User
-
+  
   # (Optional) Add your SSH KEY
-  sshkeys = var.sshkey
+  #sshkeys = var.sshkey
 
 
 }

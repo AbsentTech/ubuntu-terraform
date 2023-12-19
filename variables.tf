da6a124860d8037b190df084226fac5432763f47
Jessie Smith <109402946+jester2015@users.noreply.github.com>
2023-12-19 09:46:47 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -91,14 +91,3 @@ variable "vmid" {
   type = number
   default = 0
 }
-variable "proxmox_api_url" {
-  type = string
-}
-
-variable "proxmox_api_token_id" {
-  type = string
-}
-
-variable "proxmox_api_token_secret" {
-  type = string
-}

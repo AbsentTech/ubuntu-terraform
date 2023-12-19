2d1bba99886f5a13b4fa0fb88252186370038ff8
Jessie Smith <109402946+jester2015@users.noreply.github.com>
2023-12-19 09:34:07 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,3 +24,70 @@ variable "vpc_name" {
   description = "Set VPC name without special characters"
   default = "name"
 }
+variable "target_node" {
+  type = string
+}
+
+variable "vm_name" {
+  type = string
+}
+
+variable "cores" {
+  type = number
+}
+
+variable "memory" {
+  type = number
+}
+
+variable "ciuser" {
+  type = string
+  default = "absentadmin"
+}
+
+variable "cipassword" {
+  type = string
+  default = ""
+  
+}
+
+variable "tags" {
+  type = string
+  default = "terraform,automation"
+}
+variable "sshkey" {
+  type = string
+  default = ""
+}
+variable "ipconfig0" {
+  type = string
+}
+
+variable "disk_size" {
+  type = string
+  default = "10G"
+}
+
+variable "network_tag" {
+  type = number
+  default = 0
+}
+variable "pool" {
+  type = string
+  default = "Unassigned"
+}
+
+variable "vm_description" {
+  type = string
+  default = "Created by Terraform"
+}
+  
+variable "cloneTemplate" {
+  type = string
+  default = "UbuntuTemp"
+}
+  
+variable "vmid" {
+  type = number
+  default = 0
+}

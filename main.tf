d617d88dced6eb7d3d347c876114dc1e24237c64
Jessie Smith <109402946+jester2015@users.noreply.github.com>
2023-12-19 09:33:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,73 +3,7 @@
 # Create a new VM from a clone
 
 
-variable "target_node" {
-  type = string
-}
 
-variable "vm_name" {
-  type = string
-}
-
-variable "cores" {
-  type = number
-}
-
-variable "memory" {
-  type = number
-}
-
-variable "ciuser" {
-  type = string
-  default = "absentadmin"
-}
-
-variable "cipassword" {
-  type = string
-  default = ""
-  
-}
-
-variable "tags" {
-  type = string
-  default = "terraform,automation"
-}
-variable "sshkey" {
-  type = string
-  default = ""
-}
-variable "ipconfig0" {
-  type = string
-}
-
-variable "disk_size" {
-  type = string
-  default = "10G"
-}
-
-variable "network_tag" {
-  type = number
-  default = 5
-}
-variable "pool" {
-  type = string
-  default = "Unassigned"
-}
-
-variable "vm_description" {
-  type = string
-  default = "Created by Terraform"
-}
-  
-variable "cloneTemplate" {
-  type = string
-  default = "UbuntuTemp"
-}
-  
-variable "vmid" {
-  type = number
-  default = 0
-}
 resource "proxmox_vm_qemu" "absentvm" {
 
   target_node = var.target_node
